81fa9defb81734b23c6d1bc1cb651f2f4a0fd405
jaitken <jaitken97@gmail.com>
2024-11-12 13:35:13 -0500

    generating grid dynamically depending on number of rows and cols

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
+/* eslint-disable */
+
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const [numRows, setNumRows] = useState(4);
+  const [numCols, setNumCols] = useState(4);
+  const [selectedSquares, setSelectedSquares] = useState(['0,0', '3,3'])
+
+  const grid = [];
+  const displayGrid = [];
+
+  for (let r = 0; r < numRows; r++) {
+    grid[r] = [];
+    for (let c = 0; c < numCols; c++) {
+      grid[r][c] = r+','+c;
+      displayGrid.push(<div className={selectedSquares.includes(grid[r][c]) ? 'gridSquare selected' : 'gridSquare'} key={(r+' '+c)}>{grid[r][c]}</div>)
+    }
+    displayGrid.push(<br key={(r+' br')}></br>)
+  }
+
   return (
     <div>
-    <div className='inputArea'>
-      INPUT AREA
-    </div>
-    <div className='mainGrid'>
-      <div className='gridSquare'>grid Square</div>
-      <div className='gridSquare'>grid Square</div>
-      <div className='gridSquare'>grid Square</div>
-      <div className='gridSquare'>grid Square</div>
-    </div>
+      <div className="inputArea">INPUT AREA</div>
+      <div className="mainGrid">
+        {displayGrid.map((item)=> item)}
+      </div>
     </div>
   );
 }
